c7ec2acfabdbe46112e006670d39c0d68b9dc5d6
Bibekdhkl <bibekdh2016@gmail.com>
2020-09-22 14:21:34 +0545

    Program to find Maximum of Two Numbers

---
+++ MaximumNum/MaxmOfTwoNum.cpp
@@ -0,0 +1,17 @@
+//Program to find Maximum of 2 Numbers
+#include <iostream>
+
+using namespace std;//container where identifier like cin cout are defined
+
+int main(){
+    int i,j;
+    cout<<"Enter Two Number";
+    cin>>i>>j;
+    if (i>j){
+        cout<<i<<" is greatest";
+    }
+    else{
+        cout<<j<<" is greatest";
+    }
+    return 0;
+}
