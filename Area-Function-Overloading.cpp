3731e70e81bdea59a8764291bcdd85789076b0d0
Bibekdhkl <bibekdh2016@gmail.com>
2020-10-02 00:18:29 +0545

    Finding Area by using Function Overloading technique
    In this function overloading technique unlike C we can use same name in functions with diffent sequence in arguments

---
+++ Area-Function-Overloading.cpp
@@ -0,0 +1,37 @@
+/*
+Q.Finding area of cube cylinder and rectangle by using function overloading technique?
+
+Simply, Function overloading means functions having same name but does different task;
+As per the type and sequence of arguments.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.14 //semicolon isn't used in defining macro
+
+
+int area(int l)
+{
+    return (6 * l);
+}
+
+double area(double r, int h)
+{
+    return (pi * r * r * h);
+}
+
+int area(int l, int b)
+{
+    return (l + b);
+}
+
+int main()
+{
+    cout << "Area of cube is:" << area(2) << endl;
+    cout << "Area of cylinder is:" << area(3.2, 4) << endl; //
+    cout << "Area of rectangel is:" << area(3, 4);
+    return 0;
+}
