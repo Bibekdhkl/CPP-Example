42ad3e96a78c722007fef39f8b21f12de20bb146
Bibekdhkl <bibekdh2016@gmail.com>
2020-10-02 00:39:05 +0545

    Finding maximum of two and three number
    Used Function Overloading technique of CPP

---
+++ MaxmOfTwoNum-Function-Overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int maximum(int a, int b)
+{
+    return((a>b)?a:b);//Used conditional operator here
+}
+
+int maximum(int a, int b, int c)
+{
+    if ((a > b) && (a > c))
+        return a;
+    else if (b > c)
+        return b;
+    else
+        return c;
+}
+
+int main()
+{   int a,b,c;
+    cout<<"Enter 3 numbers";
+    cin>>a>>b>>c;
+    cout << "Maxm of 2 number is:" << maximum(a,b) << endl;
+    cout << "Maxm of 3 number is:" << maximum(a,b,c);
+    return 0;   
+}
