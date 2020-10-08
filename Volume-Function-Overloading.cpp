4c3aaaab131fba74c7f49dcd12613486665b6ee7
Bibekdhkl <bibekdh2016@gmail.com>
2020-10-08 21:03:43 +0545

    Added rectangular box volume

--- Volume-Function-Overloading.cpp
+++ Volume-Function-Overloading.cpp
@@ -7,14 +7,19 @@ double volume(double r, int h)
     return (3.14 * r * r * h);
 }
 
-double volume(int r, int h)//Always be careful about the return type
+float volume(int r, float h)//we must be conscious about return type
 {
     return ((1 / 3) * 3.14 * r * r * h);
 }
 
+int volume(int l, int b){
+    return(l * b);
+}
+
 int main()
 {
-    cout<< "Volume of cylinder is: "<<volume(2.5,4)<<endl;
-    cout << "volume of cone is :" << volume(2,4);
+    cout<< "Volume of cylinder is: "<<volume(2.5, 4)<<endl;
+    cout << "Volume of cone is :" << volume(2.2, 4.2)<<endl;
+    cout <<"Volume of rectangular box is: " << volume(2, 3);
     return 1;
 }
