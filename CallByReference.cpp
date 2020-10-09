a7280918c945f2121d205d5890fdd12b7851c4c8
Bibekdhkl <bibekdh2016@gmail.com>
2020-10-09 22:56:54 +0545

    Committed so as to make trak

--- CallByReference.cpp
+++ CallByReference.cpp
@@ -1,23 +1,22 @@
-
 //example for call by reference
 #include <iostream>
 
 using namespace std;
 
-void computeSphere(float &V, float &s, float &r){
-    V=((4/3)*3.14*r*r*r);
-    s=(4*3.14*r*r);
+void computeSphere(float &V, float &s, float &r)
+{
+    V = ((4 / 3) * 3.14 * r * r * r);
+    s = (4 * 3.14 * r * r);
 }
 
 int main()
 {
-    float radius,volume,surface;
-    cout<<"Enter radius of sphere";
-    cin>>radius;
-    computeSphere(volume,surface,radius);
-    cout<<"Volume is:"<<volume<<endl;
-    cout<<"Surface Area is:"<<surface;
-    
+    float radius, volume, surface;
+    cout << "Enter radius of sphere";
+    cin >> radius;
+    computeSphere(volume, surface, radius);
+    cout << "Volume is:" << volume << endl;
+    cout << "Surface Area is:" << surface;
 
     return 0;
 }
